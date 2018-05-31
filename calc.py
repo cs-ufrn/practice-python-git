ece5eea9cc55ba5f4f29a440ad740fac05d676b1
Vitor Greati <greati@ufrn.edu.br>
2018-05-30 23:49:30 -0300

    Skeleton code for Python calc

---
+++ calc.py
@@ -0,0 +1,80 @@
+# This file provides a simple command-line calculator, 
+# with implementations of important mathematical functionalities.
+#
+# The implementors must fill the empty functions (all of them!)
+# with the right code. Also, there are some functions specified only in comments, 
+# without their declarations, so one should declare 
+# and implement them.
+#
+# All functions must be properly documented.
+#
+# CS UFRN 2018
+###################################################################################
+
+
+def display_calculator():
+    ''' Interact with the user, showing a command-line menu and
+        receiving input for operations and operands. '''
+    pass
+
+
+def mean(values):
+    ''' Computes the mean of a given list of numbers.'''
+    pass
+
+def median(values):
+    ''' Computes the median of a given list of numbers. '''
+    pass
+
+# mode 
+#
+# Takes a list and computes the mode value.
+
+# standard_deviation  
+#
+# Takes a list and computes the standard deviation of its values.
+
+# n_max_numbers
+#
+# Takes an integer N and a list L, and computes the N maximum numbers in L.
+
+# n_min_numbers
+#
+# Takes an integer N and a list L, and computes the N minimum numbers in L.
+
+# sum_n_max_numbers
+#
+# Takes an integer N and a list L, and computes the sum of the N maximum numbers in L.
+
+# sum_n_min_numbers
+#
+# Takes an integer N and a list L, and computes the sum of the N minimum numbers in L.
+
+def validate_matrix(lists):
+    ''' Checks if a list of lists is in fact a matrix:
+        a matrix must have lines with the same number of columns,
+        but a list of lists may disobey such property.
+    '''
+    pass
+
+def sum_matrices(matrix1, matrix2):
+    ''' Computes the sum of two matrices. '''
+    pass
+
+def multiply_matrices(matrix1, matrix2):
+    ''' Computes the product of two matrices. '''
+    pass
+
+# exp_approx
+#
+# Takes a value X and a float E, and computes an approximation for e^X with maximum error E.
+
+# sin_approx
+#
+# Takes a value X and a float E, and computes an approximation for sin(X) with maximum error E.
+
+# cos_approx
+#
+# Takes a value X and a float E, and computes an approximation for cos(X) with maximum error E.
+
+
