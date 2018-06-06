2ad0ca7d2e06f49a09e2ddc99949a4473ca63a75
Carlos Vieira <vieiramecarlos@gmail.com>
2018-06-05 23:33:16 -0300

    implement matrix verification, addition and multiplication

--- calc.py
+++ calc.py
@@ -47,13 +47,43 @@ def median(values):
 # Takes an integer N and a list L, and computes the sum of the N minimum numbers in L.
 
 def validate_matrix(lists):
-    pass
+    try:
+        columns = len(lists[0])
 
-def sum_matrices(matrix1, matrix2):
-    pass
+        for row in lists[1:]:
+            if len(row) != columns:
+                return False
+        return True
+    except (TypeError, LookupError):
+        err = "lists argument is not 2-dimensional list"
+        raise TypeError(err)
+
+def add_matrices(matrix1, matrix2):
+    if not validate_matrix(matrix1) or not validate_matrix(matrix2):
+        raise TypeError("argument is not a valid matrix")
+    if len(matrix1) != len(matrix2) or len(matrix1[0]) != len(matrix2[0]):
+        raise ValueError("matrices do not have the same shape")
+
+    result = []
+    for m1_row, m2_row in zip(matrix1, matrix2):
+        result_row = [t1+t2 for t1,t2 in zip(m1_row, m2_row)]
+        result.append(result_row)
+    return result
 
 def multiply_matrices(matrix1, matrix2):
-    pass
+    if not validate_matrix(matrix1) or not validate_matrix(matrix2):
+        raise TypeError("argument is not a valid matrix")
+    if len(matrix1[0]) != len(matrix2):
+        raise ValueError("matrices shapes do not align")
+
+    result = []
+    for m1_row in matrix1:
+        result_row = []
+        for m2_col in zip(*matrix2):
+            r_term = sum([a*b for a,b in zip(m1_row, m2_col)])
+            result_row.append(r_term)
+        result.append(result_row)
+    return result
 
 # exp_approx
 #
